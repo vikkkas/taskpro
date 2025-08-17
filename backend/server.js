338f071e276f075203d6857da84a7f418753d6dc
Vikas Prasad <vp030217@gmail.com>
2025-08-18 00:29:04 +0530

    CORS restriction removed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: true, // Allow all origins
   credentials: true
 }));
 
